c5bd904b1e8e0a2df0d6bb8321f125e37ad69bb6
Mahdi Youseftabar <roboticsexpert@gmail.com>
2017-03-09 03:00:58 +0330

    Change getUri to uri

--- src/Data/RepositoryManager.php
+++ src/Data/RepositoryManager.php
@@ -565,7 +565,9 @@ class RepositoryManager implements RepositoryManagerInterface
             return $name;
         }
 
-        return '/'.$route->current()->getUri();
+
+
+        return '/'.$route->current()->uri;
     }
 
     /**
